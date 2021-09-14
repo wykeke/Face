6c3502667a3bce991bb27bd07c22427b38c6d046
wykeke <2565556263@qq.com>
2021-09-14 15:22:25 +0800

    face

--- app/src/main/java/com/example/face/MainActivity.kt
+++ app/src/main/java/com/example/face/MainActivity.kt
@@ -12,18 +12,23 @@ import android.media.ImageReader
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Surface
 import android.view.TextureView
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var camera_device: CameraDevice
+        fun iscamera_deviceInitialzed() = ::camera_device.isInitialized
     lateinit var texture_view_surface: Surface
     lateinit var image_reader_surface: Surface
     lateinit var capture_session: CameraCaptureSession
+    lateinit var image_reader: ImageReader
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                     return false
                 }
 
-                //表示s摄像头进入可用状态
+                //表示摄像头进入可用状态
 
                 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
                 override fun onSurfaceTextureAvailable(
@@ -60,20 +65,43 @@ class MainActivity : AppCompatActivity() {
                 }
 
             }
+        val take_photo_buutton = findViewById<Button>(R.id.button)
+        take_photo_buutton.setOnClickListener {
+            take_photo()
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun take_photo(){
+        if (iscamera_deviceInitialzed()){
+
+        }
+        val request_builder = camera_device.createCaptureRequest(
+            CameraDevice.TEMPLATE_STILL_CAPTURE
+        )
+        request_builder.addTarget(image_reader_surface)
+        val request = request_builder.build()
+        capture_session.capture(request,null,null)
+    }
+
+    val image_available = ImageReader.OnImageAvailableListener {
+        Log.i("ccccc","got a image")
+    }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun open_camera(){
 
-        val image_reader = ImageReader.newInstance(200,200,
+        image_reader = ImageReader.newInstance(200,200,
             ImageFormat.JPEG,2)
         image_reader_surface = image_reader.surface
+        image_reader.setOnImageAvailableListener(image_available,null)
 
         val camera_manager = getSystemService(
             Context.CAMERA_SERVICE
         ) as CameraManager
 
+
+        //申请权限
 //        requestPermissions(arrayOf(Manifest.permission.CAMERA),11)
 
         if (ActivityCompat.checkSelfPermission(
